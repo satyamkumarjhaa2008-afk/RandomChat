0d7a6efc069d19a442301382c9ea89dbfd59f213
satyamkumarjhaa2008-afk <satyamkumarjhaa2008@gmail.com>
2026-09-15 23:04:47 +0530

    chore: add ESLint configuration

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+// eslint.config.js
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  { ignores: ['dist','node_modules'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  { files:['**/*.{ts,tsx}'], languageOptions:{globals:{...globals.browser}}, plugins:{'react-hooks':reactHooks,'react-refresh':reactRefresh}, rules:{'react-hooks/rules-of-hooks':'error','react-hooks/exhaustive-deps':'off','react-refresh/only-export-components':'warn'} }
+);
